c35fd5f1742dda9408bd5bf0aed21c488505b86c
CTRL+N <maupetit.sylvain@gmail.com>
2016-10-10 11:00:02 +0200

    Update SlideshowModule
    - ajout du setdir et setfilename  dans le  __construct()

--- SlideshowModule/SlideshowModule.php
+++ SlideshowModule/SlideshowModule.php
@@ -2,29 +2,20 @@
 
 namespace CTRLPlusN\Modules\SlideshowModule;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use CTRLPlusN\Extensions\FileSystem\FileSystemTools;
 use CTRLPlusN\Modules\SlideshowModule\Model\Slideshow;
 
 class SlideshowModule {
 
-    const FILE = 'settings-slideshow.json';
-    const LOC_PATH = "@CTRLPlusN/Resources/json";
-
-    /**
-     * ContainerInterface
-     */
-    protected $container;
-    
     /**
      * FileSystemTools
      */
     protected $fst;
 
-    public function __construct(ContainerInterface $container) {
-        $this->container = $container;
+    public function __construct() {
         $this->fst = FileSystemTools::create();
-        $this->fst->setRelativePath('../src/CTRLPlusN/Resources/json', 'settings-slideshow.json');
+        $this->fst->setDir('../src/CTRLPlusN/Resources/json');
+        $this->fst->setFilename('settings-slideshow.json');
     }
 
     public function loadConfiguration() {
